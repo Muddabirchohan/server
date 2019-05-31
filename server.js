3685caa97b13e7bff250b7b711ad5f5eba62a630
Muddabirchohan <muddabir22@gmail.com>
2019-05-31 16:29:57 +0500

    ss

--- server.js
+++ server.js
@@ -1,30 +1,119 @@
-const http = require('http');
-const port = process.env.port || 8000;
-const app = require('./index');
+const express = require('express');
+const app = express();
+const registerCustomer = require('./api/routes/customerRegistrations');
+const registerSeller = require('./api/routes/sellerRegistrations');
+const productRoutes = require('./api/routes/products');
+const CustomerBuyRequest = require('./api/routes/customerBuyRequests');
+const CustomerNewRequest = require('./api/routes/customerNewRequest');
+const CustomerBugRequest = require('./api/routes/customerBugRequest');
+const CustomerCustomizationRequest = require('./api/routes/customerCustomization');
+const ForgotPassword = require('./api/routes/ForgotPassword');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
 require("dotenv").config();
 const path = require("path");
 
-// ... other app.use middleware 
-app.use(express.static(path.join(__dirname, "client", "build")))
+
+mongoose.connect('mongodb://localhost/sbay' ,{ useMongoClient: true });
+mongoose.Promise = global.Promise;
+
+// let gfs;
+// conn.once('open',() => {
+// gfs = Grid(conn.db,mongoose.mongo);
+// gfs.collection('uploads');
+// })
+
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+
+    app.use(allowCrossDomain);
+    app.use(express.static(path.join(__dirname, "client", "build")))
 
 // ...
 // Right before your app.listen(), add this:
+
+
+
+app.use(morgan('dev'));
+app.use(bodyparser.urlencoded({ extended: false}));
+app.use(bodyparser.json());
+app.use(express.static('uploads'));
+// app.use(methodoverride,'_method');
+
+//routes
+app.use("/customers", registerCustomer);
+app.use("/buyRequest", CustomerBuyRequest);
+app.use("/newRequest", CustomerNewRequest);
+app.use("/customRequest",CustomerCustomizationRequest);
+app.use("/bugRequest", CustomerBugRequest);
+app.use("/sellers",   registerSeller );
+app.use("/products",productRoutes);
+// app.use("/forgot",ForgotPassword);
+
+app.use(cors({
+    methods: ['GET','POST','PUT','PATCH'],
+    credentials: true, origin: true,
+}))
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+
+    // res.header("Access-Control-Allow-Origin","*");
+    // res.header(
+        
+    //     "Access-Control-Allow-Headers",
+    //     "Origin, X-Requested-With, Content-Type, Accept , Authorization"  
+    // );
+    // if(req.method === "OPTIONS"){
+    //     res.header('Access-Control-Allow-Methods',"GET,PUT,POST,PATCH,DELETE");
+    //     return res.status(200).json({});
+    // }
+    // next();
+})
+
+// Routes which should handle requests
+
+
+// app.use(cors({
+//     'allowedHeaders': ['sessionId', 'Content-Type'],
+//     'exposedHeaders': ['sessionId'],
+//     'origin': '*',
+//     'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     'preflightContinue': false
+//   }));
+
+app.use((req,res,next)=> {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error)
+})
+
+
+app.use((error,req,res,next)=> {
+    res.status(error.status || 7000);
+    res.json({
+        error: {
+        message: error.message
+        }
+    })
+})
+
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-const server = http.createServer(app);
+app.listen(port,function(){
+    console.log("now listening for requests");
+})
+module.exports = app;
 
-server.listen(port);
 
-
-
-
-// http.createServer(function (request, response) {
-//     response.writeHead(200, {
-//         'Content-Type': 'application/json',
-//         'Access-Control-Allow-Origin' : '*',
-//         'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
-//     });
-//     response.end('Hello World\n');
-//     }).listen(port);
